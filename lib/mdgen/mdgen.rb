56ca678b38375f497b98b6a57e51b69c3b07e205
edhowland <ed.howland@gmail.com>
2015-06-18 00:52:15 +0000

    \'process method executes supplied block in context of the object MdGen\'

--- lib/mdgen/mdgen.rb
+++ lib/mdgen/mdgen.rb
@@ -1,5 +1,19 @@
 # mdgen.rb - MdGen class DSL to generate Markdown
 
 class MdGen
+  def iniialize
 
+  end
+
+  def hello
+    puts 'hello'
+  end
+
+  def world
+  puts 'world'
+  end
+
+def process(&blk)
+    self.instance_exec &blk
+  end  
 end
