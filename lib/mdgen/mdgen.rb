ded3e8d71143fc09b5d00a1e1371e92b2e2341f8
edhowland <ed.howland@gmail.com>
2015-06-18 01:11:16 +0000

    \'added h1, h2, code, text and attr_reader to mdgen.rb\'

--- lib/mdgen/mdgen.rb
+++ lib/mdgen/mdgen.rb
@@ -1,16 +1,25 @@
 # mdgen.rb - MdGen class DSL to generate Markdown
 
 class MdGen
-  def iniialize
-
+  def initialize
+    @codes = []
   end
 
-  def hello
-    puts 'hello'
+  attr_reader :codes
+  def h1 string
+    @codes << [:h1, string]
   end
 
-  def world
-  puts 'world'
+  def h2 string
+    @codes << [:h2, string]
+  end
+
+  def code string
+    @codes << [:code, string]
+  end
+
+def text string
+    @codes << [:text, string]
   end
 
 def process(&blk)
