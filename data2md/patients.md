f4e227b08678687e7ba75eba591999a45c584d8a
edhowland <ed.howland@gmail.com>
2015-06-18 20:51:52 +0000

    \'added patients2md.rb patients.html patients.md patients.pdf\'

---
+++ data2md/patients.md
@@ -0,0 +1,11 @@
+# PATIENT REPORT
+
+## Patients Names
+
+1. edhowland
+2. rwcitek
+
+## Footer
+
+Copyright 2015, Me
+
--- data2md/patients2md.rb
+++ data2md/patients2md.rb
@@ -6,7 +6,7 @@ require '../lib/sqlite'
 markdown do
   h1 'PATIENT REPORT'
   h2 'Patients Names'
-  ordered_list patients_names('../db/test.db')
+  ordered_list patients_names('../db/test.db').map {|e| e[0]}
 
 h2 'Footer'
   text 'Copyright 2015, Me'
