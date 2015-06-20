6867d81b5da2aa98ae06300d2c402bf72367c8c3
edhowland <ed.howland@gmail.com>
2015-06-20 19:41:42 +0000

    \'added embedded bold to input for integrate_parse_format_spec.rb\'

---
+++ spec/integrate_parse_format_spec.rb
@@ -0,0 +1,30 @@
+# integrate_parse_format_spec.rb - spec for both TextParse, TextFormat
+
+require '../lib/mdgen'
+require 'minitest/autorun'
+
+describe 'Format a Parsed input string' do
+  before do
+    @p=TextParse.new
+    @f = TextFormat.new
+  end
+
+  describe 'empty input' do
+    subject { @f.format @p.parse('') }
+
+    specify { subject.must_equal '' }
+  end
+
+  describe 'simple text input' do
+    subject { @f.format @p.parse('text') }
+
+    specify { subject.must_equal 'text' }
+  end
+
+  describe "embedded bold 'this is [bold vold] text'" do
+    subject { @f.format @p.parse('this is [bold bold] text') }
+
+    specify { subject.must_equal 'this is **bold** text' }
+  end
+
+end
