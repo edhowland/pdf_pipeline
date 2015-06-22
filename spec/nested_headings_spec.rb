1a6bba6e1a2c0feda6898d54febb84b4197a0e38
edhowland <ed.howland@gmail.com>
2015-06-22 14:05:16 +0000

    \'added specify for 6 nested levels for headings\'

--- spec/nested_headings_spec.rb
+++ spec/nested_headings_spec.rb
@@ -31,5 +31,14 @@ end
       end
     end
 
+    specify { subject.must_equal [
+      [:h1, 'h1'],
+      [:h2, 'h2'],
+      [:h3, 'h3'],
+      [:h4, 'h4'],
+      [:h5, 'h5'],
+      [:h6, 'h6']
+
+      ] }
   end
 end
