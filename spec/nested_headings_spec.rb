9e900629d31918ab75943ccacdcafb9b78956e5c
edhowland <ed.howland@gmail.com>
2015-06-22 15:37:55 +0000

    \'7 nested levels of headings raises MdGen::NestingToDeep\'

--- spec/nested_headings_spec.rb
+++ spec/nested_headings_spec.rb
@@ -56,7 +56,7 @@ end
   end
   describe '7 nested headings raises exception' do
     subject do
-      Proc do
+      Proc.new do
       gen.process do
         h 1, 'h1' do |l|
             h l, 'h2' do |l|
