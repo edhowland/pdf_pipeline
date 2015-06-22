25ee2e5487a5aee8ab90e1916e477205071b7dee
edhowland <ed.howland@gmail.com>
2015-06-22 02:09:34 +0000

    \'describe for 2 nested headdings\'

---
+++ spec/nested_headings_spec.rb
@@ -0,0 +1,21 @@
+# nested_headings_spec.rb - spec for markdownnested blocks
+
+require '../lib/mdgen'
+require 'minitest/autorun'
+
+describe 'Nested Heading' do
+  let(:gen) { MdGen.new }
+
+  describe 'no nested' do
+    subject { gen.process { h 'head' } }
+
+  specify { subject.must_equal [[:h1, 'head']] }
+  end
+
+  describe 'two nests' do
+    subject do 
+      gen.process { h('head1') { h('head2')} }
+    end
+
+end
+end
